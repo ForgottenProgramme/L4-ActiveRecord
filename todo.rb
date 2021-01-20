27b17e5953ecb3510709882e39eb6bff0790a6e4
ForgottenProgramme <forgotten.programme@gmail.com>
2021-01-21 02:27:26 +0530

    main todo file

--- todo.rb
+++ todo.rb
@@ -1,5 +1,5 @@
 require "active_record"
-# todo.rb
+
 class Todo < ActiveRecord::Base
   def due_today?
     due_date == Date.today
@@ -8,10 +8,37 @@ class Todo < ActiveRecord::Base
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
-    "#{display_status} #{todo_text} #{display_date}"
+    " #{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
   def self.to_displayable_list
     all.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts all.where("due_date < :date", { date: Date.today }).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Today\n"
+    all.where(due_date: Date.today).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts all.where("due_date > :date", { date: Date.today }).to_displayable_list
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(id)
+    todo = find(id)
+    todo.completed = true
+    todo.save
+    return todo
+  end
+
+  def self.add_task(task)
+    create!(completed: false, todo_text: task[:todo_text], due_date: Date.today + task[:due_in_days])
   end
 end
